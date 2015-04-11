e217406ff4df747cf42bbb99147367f5e4dd5b8b
Simon Lepel <simbo@simbo.de>
2015-04-12 01:14:13 +0200

    add templates for javascript, stylus and html

---
+++ templates/src/assets-dev/js/main.js
@@ -0,0 +1,13 @@
+(function($, window, document, undefined) {
+    'use strict';
+
+    var $window = $(window),
+        $document = $(document);
+
+    $document.on({
+        ready: function(ev) {
+
+        }
+    });
+
+})(jQuery, window, document);
