318bf2083ab31a05fee30255ef98b263eef0d555
Simon Lepel <simbo@simbo.de>
2015-04-18 16:21:50 +0200

    some refactoring

--- Slushfile.js
+++ Slushfile.js
@@ -10,7 +10,8 @@
 
 
 // required modules
-var autoPlug = require('auto-plug'),
+var _        = require('lodash'),
+    autoPlug = require('auto-plug'),
     chalk    = require('chalk'),
     fs       = require('fs'),
     gulp     = require('gulp'),
@@ -21,15 +22,14 @@ var autoPlug = require('auto-plug'),
     util     = require('util');
 
 
-// main variables
+// some variables
 var cwd = process.cwd(),
     pkgDir = __dirname,
     pkgJson = require(path.join(pkgDir, 'package.json')),
     home = process.env.HOME || process.env.HOMEPATH || process.env.USERPROFILE,
     templates = path.join(pkgDir, 'templates'),
     savedDataFile = path.join(home, '.' + pkgJson.name),
-    savedDataFileExists = fs.existsSync(savedDataFile),
-    savedData = savedDataFileExists ? ini.parse(fs.readFileSync(savedDataFile, 'utf-8')) : { author: {} };
+    savedData = fs.existsSync(savedDataFile) ? ini.parse(fs.readFileSync(savedDataFile, 'utf-8')) : false;
 
 
 // auto-require plugins
@@ -49,35 +49,30 @@ var params = (function () {
 // default options
 var defaultOptions = (function () {
         var options = {
-            author: {
-                name: savedData.author.name || path.basename(home),
-                email: savedData.author.email || '',
-                url: savedData.author.url || '',
-                githubUser: savedData.author.githubUser || path.basename(home)
-            },
-            project: {
-                name: path.basename(cwd),
-                license: 'MIT',
-                version: '0.1.0'
-            },
-            options: {
-                useVagrant: true,
-                webserver: 'nginx',
-                website: 'html',
-                php: false,
-                database: 'none',
-                phpmyadmin: false,
-                supportTravis: true,
-                vagrantUp: false,
-                runInstall: false
-            }
+            authorName: savedData ? savedData.authorName : path.basename(home),
+            authorEmail: savedData ? savedData.authorEmail : '',
+            authorUrl: savedData ? savedData.authorUrl : '',
+            authorGithubUser: savedData ? savedData.authorGithubUser : path.basename(home),
+            projectName: path.basename(cwd),
+            projectDescription: '',
+            projectLicenseType: 'MIT',
+            projectVersion: '0.1.0',
+            optionVagrant: true,
+            optionWebserver: 'nginx',
+            optionWebsite: 'html',
+            optionPhp: false,
+            optionDatabase: 'none',
+            optionPhpmyadmin: false,
+            optionTravis: true,
+            optionVagrantUp: false,
+            optionInstall: false
         };
-        if (!savedDataFileExists) {
+        if (!savedData) {
             var gitconfig = path.join(home, '.gitconfig');
             if (fs.existsSync(gitconfig)) {
                 var gitconfigData = ini.parse(fs.readFileSync(gitconfig, 'utf-8'));
-                options.author.name = gitconfigData.user.name;
-                options.author.email = gitconfigData.user.email;
+                options.authorName = gitconfigData.user.name;
+                options.authorEmail = gitconfigData.user.email;
             }
         }
         return options;
@@ -86,106 +81,102 @@ var defaultOptions = (function () {
 
 // inquirer questions
 var prompts = [{
-        when: function (answers) {
-            return savedDataFileExists;
-        },
-        type: 'confirm',
         name: 'useSavedAuthorData',
+        type: 'confirm',
+        when: function (answers) {
+            return savedData ? true : false;
+        },
         message: function (answers) {
             return 'Use this as author data?' +
-                chalk.reset.gray('\nName: ') + chalk.yellow(savedData.author.name) +
-                chalk.reset.gray('\nE-Mail: ') + chalk.yellow(savedData.author.email) +
-                chalk.reset.gray('\nURL: ') + chalk.yellow(savedData.author.url) +
-                chalk.reset.gray('\nGitHub User: ') + chalk.yellow(savedData.author.githubUser) +
+                chalk.reset.gray('\nName: ') + chalk.yellow(savedData.authorName) +
+                chalk.reset.gray('\nE-Mail: ') + chalk.yellow(savedData.authorEmail) +
+                chalk.reset.gray('\nURL: ') + chalk.yellow(savedData.authorUrl) +
+                chalk.reset.gray('\nGitHub User: ') + chalk.yellow(savedData.authorGithubUser) +
                 '\n';
         }
     }, {
-        when: function (answers) {
-            return !savedDataFileExists || !answers.useSavedAuthorData;
-        },
         name: 'authorName',
+        when: function (answers) {
+            return !savedData || !answers.useSavedAuthorData;
+        },
         message: 'What is the ' + chalk.yellow('author\'s name') + '?',
-        default: defaultOptions.author.name
+        default: defaultOptions.authorName
     }, {
-        when: function (answers) {
-            return !savedDataFileExists || !answers.useSavedAuthorData;
-        },
         name: 'authorEmail',
+        when: function (answers) {
+            return !savedData || !answers.useSavedAuthorData;
+        },
         message: 'What is the ' + chalk.yellow('author\'s email') + '?',
-        default: defaultOptions.author.email
+        default: defaultOptions.authorEmail
     }, {
-        when: function (answers) {
-            return !savedDataFileExists || !answers.useSavedAuthorData;
-        },
         name: 'authorUrl',
+        when: function (answers) {
+            return !savedData || !answers.useSavedAuthorData;
+        },
         message: 'What is the ' + chalk.yellow('author\'s website') + '?',
-        default: defaultOptions.author.url
+        default: defaultOptions.authorUrl
     }, {
+        name: 'authorGithubUser',
         when: function (answers) {
-            return !savedDataFileExists || !answers.useSavedAuthorData;
+            return !savedData || !answers.useSavedAuthorData;
         },
-        name: 'githubUser',
         message: 'What is the ' + chalk.yellow('GitHub username') + '?',
-        default: defaultOptions.author.githubUser
+        default: defaultOptions.authorGithubUser
     }, {
-        when: function (answers) {
-            return !savedDataFileExists || !answers.useSavedAuthorData;
-        },
         name: 'saveAuthorData',
         type: 'confirm',
-        message: 'Do you want to ' + chalk.yellow('save author information') + ' for future project scaffolding?'
+        when: function (answers) {
+            return !savedData || !answers.useSavedAuthorData;
+        },
+        message: 'Do you want to ' + chalk.yellow('save author information') + ' for future project scaffolding?',
+        default: true
     }, {
         name: 'projectName',
         message: 'What is the ' + chalk.yellow('name') + ' of your project?',
-        default: defaultOptions.project.name
+        default: defaultOptions.projectName
     }, {
         name: 'projectDescription',
         message: 'What is the ' + chalk.yellow('description') + ' of your project?',
+        default: defaultOptions.projectDescription
     }, {
         name: 'projectVersion',
         message: 'What is the ' + chalk.yellow('version') + ' of your project?',
-        default: defaultOptions.project.version
+        default: defaultOptions.projectVersion
     }, {
-        name: 'license',
+        name: 'projectLicenseType',
         message: 'What ' + chalk.yellow('license') + ' do you want to use for your project?',
-        default: 'MIT'
+        default: defaultOptions.projectLicenseType
     }, {
-        name: 'licenseUrl',
+        name: 'projectLicenseUrl',
         message: 'What is the ' + chalk.yellow('url for license') + ' information?',
         default: function (answers) {
-            return getLicenseUrl(answers.license.toLowerCase());
+            return getLicenseUrl(answers.projectLicenseType);
         }
     }, {
-        name: 'repository',
+        name: 'projectRepositoryUrl',
         message: 'What is the url of the project ' + chalk.yellow('repository') + '?',
         default: function (answers) {
-            var githubUser = savedData.githubUser || defaultOptions.author.githubUser || answers.githubUser;
-            if (githubUser) {
-                return getGithubUrl(githubUser, answers.projectName);
-            }
-            return '';
+            var githubUser = savedData.authorGithubUser || defaultOptions.authorGithubUser || answers.authorGithubUser;
+            return githubUser ? getGithubUrl(githubUser, answers.projectName) : '';
         }
     }, {
-        name: 'bugs',
+        name: 'projectBugtrackerUrl',
         message: 'What is the url of the project ' + chalk.yellow('bug tracker') + '?',
         default: function (answers) {
-            var githubUser = savedData.githubUser || defaultOptions.author.githubUser || answers.githubUser;
-            if (githubUser) {
-                return getGithubUrl(githubUser, answers.projectName, 'issues');
-            }
-            return '';
+            var githubUser = savedData.authorGithubUser || defaultOptions.authorGithubUser || answers.authorGithubUser;
+            return githubUser ? getGithubUrl(githubUser, answers.projectName, 'issues') : '';
         }
     }, {
+        name: 'optionVagrant',
         type: 'confirm',
-        name: 'useVagrant',
         message: 'Do you want to use ' + chalk.yellow('Vagrant') + '?',
-        default: defaultOptions.options.useVagrant
+        default: defaultOptions.optionVagrant
     }, {
-        when: function(answers) {
-            return answers.useVagrant;
-        },
+        name: 'optionWebserver',
         type: 'list',
-        name: 'webserver',
+        when: function(answers) {
+            return answers.optionVagrant;
+        },
         message: 'What kind of ' + chalk.yellow('webserver') + ' do you want to use?',
         choices: [{
             name: 'nginx',
@@ -198,22 +189,22 @@ var prompts = [{
             value: 'connect'
         }],
         default: function(answers) {
-            return answers.useVagrant ? defaultOptions.options.webserver : 'connect'
+            return answers.optionVagrant ? defaultOptions.optionWebserver : 'connect'
         }
     }, {
-        when: function(answers) {
-            return ['nginx', 'apache'].indexOf(answers.webserver)!==-1;
-        },
+        name: 'optionPhp',
         type: 'confirm',
-        name: 'php',
-        message: 'Do want to install ' + chalk.yellow('PHP') + '?',
-        default: defaultOptions.options.php
-    }, {
         when: function(answers) {
-            return answers.useVagrant;
+            return ['nginx', 'apache'].indexOf(answers.optionWebserver)!==-1;
         },
+        message: 'Do want to install ' + chalk.yellow('PHP') + '?',
+        default: defaultOptions.optionPhp
+    }, {
+        name: 'optionDatabase',
         type: 'list',
-        name: 'database',
+        when: function(answers) {
+            return answers.optionVagrant;
+        },
         message: 'Do want to setup a ' + chalk.yellow('database') + '?',
         choices: [{
             name: 'No',
@@ -225,20 +216,20 @@ var prompts = [{
             name: 'CouchDB',
             value: 'couchdb'
         }],
-        default: defaultOptions.options.database
+        default: defaultOptions.optionDatabase
     }, {
-        when: function(answers) {
-            return answers.database==='mysql';
-        },
+        name: 'optionPhpmyadmin',
         type: 'confirm',
-        name: 'phpmyadmin',
+        when: function(answers) {
+            return answers.optionDatabase==='mysql' && answers.optionPhp;
+        },
         message: 'Do you want to install ' + chalk.yellow('phpMyAdmin') + '?',
         default: function(answers) {
-            return answers.database==='mysql' ? true : defaultOptions.options.phpmyadmin;
+            return answers.optionDatabase==='mysql' && answers.optionPhp ? true : defaultOptions.optionPhpmyadmin;
         }
     }, {
+        name: 'optionWebsite',
         type: 'list',
-        name: 'website',
         message: 'What kind of basic ' + chalk.yellow('website') + ' do you want to setup?',
         choices: [{
             name: 'Single HTML file',
@@ -247,33 +238,33 @@ var prompts = [{
             name: 'Metalsmith (static site generator)',
             value: 'metalsmith'
         }],
-        default: defaultOptions.options.website
+        default: defaultOptions.optionWebsite
     }, {
+        name: 'optionTravis',
         type: 'confirm',
-        name: 'supportTravis',
         message: 'Do you want to add ' + chalk.yellow('Travis CI') + ' support?',
-        default: defaultOptions.options.supportTravis
+        default: defaultOptions.optionTravis
     }, {
-        when: function(answers) {
-            return answers.useVagrant;
-        },
+        name: 'optionVagrantUp',
         type: 'confirm',
-        name: 'vagrantUp',
+        when: function(answers) {
+            return answers.optionVagrant;
+        },
         message: 'Do you want to run ' + chalk.yellow('vagrant up') + ' after scaffolding?',
-        default: defaultOptions.options.vagrantUp
+        default: defaultOptions.optionVagrantUp
     }, {
-        when: function(answers) {
-            return !answers.useVagrant;
-        },
+        name: 'optionInstall',
         type: 'confirm',
-        name: 'runInstall',
+        when: function(answers) {
+            return !answers.optionVagrant;
+        },
         message: 'Do you want to ' + chalk.yellow('install') + ' npm and bower packages after scaffolding?',
         default: function(ansers) {
-            return answers.useVagrant ? false : defaultOptions.options.runInstall;
+            return answers.optionVagrant ? false : defaultOptions.optionInstall;
         }
     }, {
+        name: 'continue',
         type: 'confirm',
-        name: 'moveon',
         message: 'Please check your answers. ' + chalk.yellow('Continue') + '?'
     }];
 
@@ -293,7 +284,7 @@ function getGithubUrl (user, project, type) {
 function getLicenseUrl (license) {
     switch(license.toLowerCase()) {
         case 'mit':
-            return 'http://' + defaultOptions.author.githubUser + '.mit-license.org/';
+            return 'http://' + defaultOptions.authorGithubUser + '.mit-license.org/';
         case 'gpl':
         case 'gpl3':
             return 'https://www.gnu.org/licenses/gpl-3.0.txt';
@@ -305,59 +296,29 @@ function getLicenseUrl (license) {
 }
 
 
-// parse answers, generate data object
+// parse answers, generate options object
 function parseAnswers (answers) {
-    return {
-        project: {
-            name: answers.projectName,
-            description: answers.projectDescription,
-            version: answers.projectVersion,
-        },
-        author: {
-            name: savedDataFileExists && answers.useSavedAuthorData ? savedData.author.name : answers.authorName,
-            email: savedDataFileExists && answers.useSavedAuthorData ? savedData.author.email : answers.authorEmail,
-            url: savedDataFileExists && answers.useSavedAuthorData ? savedData.author.url : answers.authorUrl,
-            githubUser: savedDataFileExists && answers.useSavedAuthorData ? savedData.author.githubUser : answers.githubUser
-        },
-        repository: {
-            type: 'git',
-            url: answers.repository
-        },
-        bugs: {
-            url: answers.backgroundImage
-        },
-        license: {
-            type: answers.license,
-            url: answers.licenseUrl,
-            year: new Date().getFullYear()
-        },
-        options: {
-            useVagrant: answers.useVagrant,
-            webserver: answers.webserver,
-            website: answers.website,
-            php: answers.php,
-            database: answers.database,
-            phpmyadmin: answers.phpmyadmin,
-            supportTravis: answers.supportTravis,
-            vagrantUp: answers.vagrantUp,
-            runInstall: answers.runInstall
-        }
-    };
+    var options = _.merge({}, answers);
+    if (savedData && answers.useSavedAuthorData) {
+        options = _.merge(options, savedData);
+    }
+    options.projectLicenseYear = new Date().getFullYear();
+    return options;
 }
 
 
 // scaffold the project
-function scaffold (data, done) {
+function scaffold (options, done) {
     gulp.src([
             templates + '/**/*',
-            ( data.options.supportTravis ? '' : '!' + templates + '/.travis.yml' )
+            ( options.optionTravis ? '' : '!' + templates + '/.travis.yml' )
         ], {dot: true})
-        .pipe(g.template(data))
+        .pipe(g.template(options))
         .pipe(g.conflict(cwd + '/'))
         .pipe(gulp.dest(cwd + '/'))
         .pipe(g.if(
-            data.options.runInstall,
-            g.install({ignoreScripts: true})
+            options.optionInstall,
+            g.install({ ignoreScripts: true })
         ))
         .on('end', function () {
             done();
@@ -368,40 +329,27 @@ function scaffold (data, done) {
 // the actual gulp task
 gulp.task('default', function (done) {
     if (params.silent) {
-        var data = parseAnswers({
-                authorName: defaultOptions.author.name,
-                authorEmail: defaultOptions.author.email,
-                authorUrl: defaultOptions.author.url,
-                githubUser: defaultOptions.author.githubUser,
-                projectName: defaultOptions.project.name,
-                projectDescription: '',
-                projectVersion: defaultOptions.project.version,
-                license: defaultOptions.project.license,
-                licenseUrl: getLicenseUrl(defaultOptions.project.license),
-                repository: getGithubUrl(defaultOptions.author.githubUser, defaultOptions.project.name),
-                bugs: getGithubUrl(defaultOptions.author.githubUser, defaultOptions.project.name, 'issues'),
-                useVagrant: defaultOptions.options.useVagrant,
-                webserver: defaultOptions.options.webserver,
-                website: defaultOptions.options.website,
-                php: defaultOptions.options.php,
-                database: defaultOptions.options.database,
-                phpmyadmin: defaultOptions.options.phpmyadmin,
-                supportTravis: defaultOptions.options.supportTravis,
-                vagrantUp: defaultOptions.options.vagrantUp,
-                runInstall: defaultOptions.options.runInstall
-            });
-        scaffold(data, done);
+        var options = _.merge({}, defaultOptions);
+        options.projectLicenseUrl = getLicenseUrl(defaultOptions.projectLicenseType);
+        options.projectRepositoryUrl = getGithubUrl(defaultOptions.authorGithubUser, defaultOptions.projectName);
+        options.projectBugtrackerUrl = getGithubUrl(defaultOptions.authorGithubUser, defaultOptions.projectName, 'issues');
+        scaffold(parseAnswers(options), done);
     }
     else {
         inquirer.prompt(prompts, function (answers) {
-            if (!answers.moveon) {
+            if (!answers.continue) {
                 return done();
             }
-            var data = parseAnswers(answers);
+            var options = parseAnswers(answers);
             if (answers.saveAuthorData) {
-                fs.writeFileSync(savedDataFile, ini.stringify({author: data.author}))
+                fs.writeFileSync(savedDataFile, ini.stringify({
+                    authorName: options.authorName,
+                    authorEmail: options.authorEmail,
+                    authorUrl: options.authorUrl,
+                    authorGithubUser: options.authorGithubUser,
+                }));
             }
-            scaffold(data, done);
+            scaffold(options, done);
         });
     }
 });
--- templates/README.md
+++ templates/README.md
@@ -1,9 +1,9 @@
-<%= project.name %>
-<% for (var i=0; i<project.name.length; i++) { %>=<% } %>
+<%= projectName %>
+<% for (var i=0; i<projectName.length; i++) { %>=<% } %>
 
-<% if (license.url!=='') { %>[<% } %>![<%= license.type %> License](http://img.shields.io/:license-<%= license.type.toLowerCase() %>-blue.svg?style=flat-square)<% if (license.url!=='') { %>](<%= license.url%>)<% } %>
-[![Dependencies Status](https://img.shields.io/david/<%= author.githubUser %>/<%= project.name %>.svg?style=flat-square)](https://david-dm.org/<%= author.githubUser %>/<%= project.name %>)
-[![devDependencies Status](https://img.shields.io/david/dev/<%= author.githubUser %>/<%= project.name %>.svg?style=flat-square)](https://david-dm.org/<%= author.githubUser %>/<%= project.name %>#info=devDependencies)<% if (options.supportTravis) { %>
-[![Travis Build Status](https://img.shields.io/travis/<%= author.githubUser %>/<%= project.name %>/master.svg?style=flat-square)](https://travis-ci.org/<%= author.githubUser %>/<%= project.name %>)<% } %><% if (project.description!=='') { %>
+<% if (projectLicenseUrl!=='') { %>[<% } %>![<%= projectLicenseType %> License](http://img.shields.io/:license-<%= projectLicenseType.toLowerCase() %>-blue.svg?style=flat-square)<% if (projectLicenseUrl!=='') { %>](<%= projectLicenseUrl%>)<% } %>
+[![Dependencies Status](https://img.shields.io/david/<%= authorGithubUser %>/<%= projectName %>.svg?style=flat-square)](https://david-dm.org/<%= authorGithubUser %>/<%= projectName %>)
+[![devDependencies Status](https://img.shields.io/david/dev/<%= authorGithubUser %>/<%= projectName %>.svg?style=flat-square)](https://david-dm.org/<%= authorGithubUser %>/<%= projectName %>#info=devDependencies)<% if (optionTravis) { %>
+[![Travis Build Status](https://img.shields.io/travis/<%= authorGithubUser %>/<%= projectName %>/master.svg?style=flat-square)](https://travis-ci.org/<%= authorGithubUser %>/<%= projectName %>)<% } %><% if (projectDescription!=='') { %>
 
-  > <%= project.description %><% } %>
+  > <%= projectDescription %><% } %>
