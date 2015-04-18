6a1e6bf5e1cf68d39f74f703d175d06e30c02e2e
Simon Lepel <simbo@simbo.de>
2015-04-19 01:34:47 +0200

    fix provisioning nginx config copy

--- templates/.provision/scripts/nginx.sh
+++ templates/.provision/scripts/nginx.sh
@@ -1,12 +1,7 @@
 install_packages nginx
 
 copy_file /etc/nginx/nginx.conf
-copy_file /etc/nginx/sites-available/vagrant
-copy_file /etc/nginx/conf.d/deny.conf
-copy_file /etc/nginx/conf.d/expires.conf
-copy_file /etc/nginx/conf.d/favicon.conf
-copy_file /etc/nginx/conf.d/gzip.conf
-copy_file /etc/nginx/conf.d/robots.conf<% if (php) { %>
+copy_file /etc/nginx/sites-available/vagrant<% if (php) { %>
 copy_file /etc/nginx/conf.d/php.conf<% } %><% if (phpmyadmin) { %>
 copy_file /etc/nginx/conf.d/phpmyadmin.conf<% } %>
 
