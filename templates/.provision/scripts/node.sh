1c10b8c30c4e728d596363a3b42c160643b6d282
Simon Lepel <simbo@simbo.de>
2015-04-18 21:53:40 +0200

    update node provisioning

--- templates/.provision/scripts/node.sh
+++ templates/.provision/scripts/node.sh
@@ -1,13 +1,13 @@
-
-echo "Installing NVM..."
-git clone https://github.com/creationix/nvm.git ~/.nvm && cd ~/.nvm && git checkout `git describe --abbrev=0 --tags`
-cd ~
+echo "Installing nvm..."
+git clone https://github.com/creationix/nvm.git ~/.nvm && cd ~/.nvm && git checkout `git describe --abbrev=0 --tags` && cd ~
 source ~/.nvm/nvm.sh
 
 echo "Installing node.js..."
 nvm install 0.12 &> /dev/null
 nvm alias default 0.12
 
-echo "Installing global node packages... (please be patient)"
+echo "Updating npm... (please be patient)"
 npm install -g npm@latest
-npm install -g gulp bower
+
+echo "Installing global node packages... (please be patient)"
+#npm install -g gulp<% if (bower) { %> bower<% } %>
